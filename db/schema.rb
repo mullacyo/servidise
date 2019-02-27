f3a180f9d1f7e0dcf9b47f6c3b537256df1d72c8
Callum Tan <mullacyo@hotmail.com>
2019-02-27 23:45:09 +0800

    Added Faker gem.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_175534) do
+ActiveRecord::Schema.define(version: 2019_02_27_153719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "task_id"
+    t.index ["task_id"], name: "index_comments_on_task_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.json "pictures"
+    t.float "price", null: false
+    t.datetime "duedate"
+    t.string "location"
+    t.integer "tags"
+    t.boolean "completion_status", default: false
+    t.boolean "payment_status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -50,4 +74,6 @@ ActiveRecord::Schema.define(version: 2019_02_26_175534) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
 end
